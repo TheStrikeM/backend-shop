6feafa74f76f6405059fe979acde520253c65f64
TheStrikeM <thestrikem@gmail.com>
2021-07-10 02:18:55 +0500

    End developing userModule

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -2,13 +2,14 @@ import {Module} from "@nestjs/common";
 import {TypeOrmModule} from "@nestjs/typeorm";
 import {UserEntity} from "./entities/user.entity";
 import CryptoModule from "../crypto/crypto.module";
+import UserRepository from "./repositories/user.repository";
 
 @Module({
     imports: [
         TypeOrmModule.forFeature([UserEntity]),
         CryptoModule
     ],
-    providers: [],
-    exports: [],
+    providers: [UserRepository],
+    exports: [UserRepository],
 })
 export default class UserModule {}
