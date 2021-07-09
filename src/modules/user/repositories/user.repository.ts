b303ba2b2b5961042dab033ba051351e7f294a56
TheStrikeM <thestrikem@gmail.com>
2021-07-10 01:43:04 +0500

    Add findById and remove methids

--- src/modules/user/repositories/user.repository.ts
+++ src/modules/user/repositories/user.repository.ts
@@ -1,9 +1,21 @@
 import {Injectable} from "@nestjs/common";
 import {ConfigService} from "@nestjs/config";
+import {InjectRepository} from "@nestjs/typeorm";
+import {Repository} from "typeorm";
+import {UserEntity} from "../entities/user.entity";
 
 @Injectable()
 export default class UserRepository {
     constructor(
         private readonly configService: ConfigService,
+        @InjectRepository(UserEntity) private usersRepository: Repository<UserEntity>,
     ) {}
+
+    findById(id: string): Promise<UserEntity> {
+        return this.usersRepository.findOne(id)
+    }
+
+    async remove(id: string): Promise<void> {
+        await this.usersRepository.delete(id)
+    }
 }
